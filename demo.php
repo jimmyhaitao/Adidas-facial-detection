2d0bfd5e5c525425ca6ef92a3d5050f5ee73665f
Jimmy Liao <jimmy.liao@ancss.org>
2017-07-13 16:55:12 +0800

    Create demo.php

---
+++ demo.php
@@ -0,0 +1,444 @@
+<?php 
+
+?>
+<html>
+<head>
+ <title> ADIDAS checkout page </title>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+      <script src="../examples/assets/js/scripts.js"></script>
+</head>
+
+<body>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="theme-color" content="#000000">
+    <meta name="robots" content="index,follow,noydir,noodp,archive,noyaca">
+    <meta name="description" content="The adidas and Kanye West collaboration: be the first to get the new adidas YEEZY BOOST 350 V2.">
+    <link rel="icon" href="//www.adidas.com/com/apps/rtq3so/vp_assets/images/favicon.ico?env=&v=18.24" type="image/x-icon">
+    <link rel="canonical" href="http://adidas.com/yeezy">
+    <link rel="image_src" href="//www.adidas.com/com/apps/rtq3so/vp_assets/images/350-v2/cream-white/share-image.jpg?env=&v=18.24"><!-- Schema.org -->
+    <meta itemprop="name" content="adidas YEEZY BOOST by Kanye West">
+    <meta itemprop="description" content="The adidas and Kanye West collaboration: be the first to get the new adidas YEEZY BOOST 350 V2.">
+    <meta itemprop="image" content="//www.adidas.com/com/apps/rtq3so/vp_assets/images/350-v2/cream-white/share-image.jpg?env=&v=18.24"><!-- Open Graph -->
+    <meta property="fb:app_id" content="159031900855798">
+    <meta property="og:site_name" content="adidas">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="adidas YEEZY BOOST by Kanye West">
+    <meta property="og:description" content="The adidas and Kanye West collaboration: be the first to get the new adidas YEEZY BOOST 350 V2.">
+    <meta property="og:url" content="http://www.adidas.com/kanye">
+    <meta property="og:image" content="//www.adidas.com/com/apps/rtq3so/vp_assets/images/350-v2/cream-white/share-image.jpg?env=&v=18.24">
+    <meta property="og:ttl" content="86400"><!-- Twitter Card -->
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="@adidas">
+    <meta name="twitter:title" content="adidas YEEZY BOOST by Kanye West">
+    <meta name="twitter:description" content="The adidas and Kanye West collaboration: be the first to get the new adidas YEEZY BOOST 350 V2.">
+    <meta name="twitter:image" content="//www.adidas.com/com/apps/rtq3so/vp_assets/images/350-v2/cream-white/share-image.jpg?env=&v=18.24">
+    <meta name="twitter:url" content="http://www.adidas.com/kanye"><!--Style files -->
+    <link href="//www.adidas.com/com/apps/rtq3so/vp_assets/css/libraries.min.css?env=&v=18.24" rel="stylesheet">
+    <link href="//www.adidas.com/com/apps/rtq3so/vp_assets/css/application.css?env=&v=18.24" rel="stylesheet"><!-- Javascript -->
+    <script src="//tags.tiqcdn.com/utag/adidas/adidasglobal/prod/utag.sync.js"></script>
+    <script src="https://www.google.com/recaptcha/api.js"></script>
+    <script src="//www.adidas.com/com/apps/rtq3so/vp_assets/js/libraries.min.js?env=&v=18.24"></script><!-- Tracking -->
+    <script>
+      window.ASSET_PATH       = "localhost";
+      window.DEVICE           = "localhost"
+      window.ENV              = "localhost";
+      window.DEMANDWARE_ENV   = "";
+      window.MARKET           = "";
+      window.MARKET_DOMAIN    = "localhost";
+      window.PLACES_API_HOST  = "localhost";
+      window.STORE_FINDER_TAG = "localhsot";
+      window.OCAPI_KEY        = "";
+      window.LANGUAGE_JSON    = "";
+      
+      var utag_data = {
+        campaign_name: "KANYE",
+        country: "US",
+        date: "150517",
+        environment: "PROD",
+        is_mobile: (DEVICE === 'mobile') ? 'TRUE' : '',
+        language: "EN",
+        page_category: "ORIGINALS",
+        page_name: "HOME",
+        page_type: "MICROSITE",
+        product_sport: "ORIGINALS",
+        site_name: "ADIDAS",
+        site_owner: "BRAND"
+      };
+      (function(a,b,c,d){
+        a='//tags.tiqcdn.com/utag/adidas/adidasglobal/prod/utag.js';
+        b=document;
+        c='script';
+        d=b.createElement(c);
+        d.src=a;
+        d.type='text/java'+c;
+        d.async=true;
+        a=b.getElementsByTagName(c)[0];
+        a.parentNode.insertBefore(d,a);
+      })();
+      
+    </script>
+
+
+
+    <script src="//www.adidas.com/com/apps/rtq3so/vp_assets/js/application.js?env=&v=18.24"></script><!-- Fonts -->
+    <style>
+          @font-face {
+          font-family: 'YeezyzTStar';
+          src: url('//www.adidas.com/com/apps/rtq3so/vp_assets/fonts/yeezy_tstar-regular-webfont.eot?env=&v=18.24');
+          src: local('☺'), url('//www.adidas.com/com/apps/rtq3so/vp_assets/fonts/yeezy_tstar-regular-webfont.woff?env=&v=18.24') format('woff'), url('//www.adidas.com/com/apps/rtq3so/vp_assets/fonts/yeezy_tstar-regular-webfont.ttf?env=&v=18.24') format('truetype');
+          font-weight: normal;
+          font-style: normal;
+      }
+      
+      @font-face {
+          font-family: 'YeezyzTStar';
+          src: url('//www.adidas.com/com/apps/rtq3so/vp_assets/fonts/yeezy_tstar-bold-webfont.eot?env=&v=18.24');
+          src: local('☺'), url('//www.adidas.com/com/apps/rtq3so/vp_assets/fonts/yeezy_tstar-bold-webfont.woff?env=&v=18.24') format('woff'), url('//www.adidas.com/com/apps/rtq3so/vp_assets/fonts/yeezy_tstar-bold-webfont.ttf?env=&v=18.24') format('truetype');
+          font-weight: bold;
+          font-style: normal;
+      }
+      
+      @font-face {
+          font-family: 'YeezyzTStar';
+          src: url('//www.adidas.com/com/apps/rtq3so/vp_assets/fonts/yeezy_tstar-light-webfont.eot?env=&v=18.24');
+          src: local('☺'), url('//www.adidas.com/com/apps/rtq3so/vp_assets/fonts/yeezy_tstar-light-webfont.woff?env=&v=18.24') format('woff'), url('//www.adidas.com/com/apps/rtq3so/vp_assets/fonts/yeezy_tstar-light-webfont.ttf?env=&v=18.24') format('truetype');
+          font-weight: 200;
+          font-style: normal;
+      }
+      
+      @font-face {
+          font-family: 'YeezyzTStar';
+          src: url('//www.adidas.com/com/apps/rtq3so/vp_assets/fonts/yeezy_tstar-heavy-webfont.eot?env=&v=18.24');
+          src: local('☺'), url('//www.adidas.com/com/apps/rtq3so/vp_assets/fonts/yeezy_tstar-heavy-webfont.woff?env=&v=18.24') format('woff'), url('//www.adidas.com/com/apps/rtq3so/vp_assets/fonts/yeezy_tstar-heavy-webfont.ttf?env=&v=18.24') format('truetype');
+          font-weight: 600;
+          font-style: normal;
+      }
+      
+      @font-face {
+          font-family: 'adineueBold';
+          src: url('//www.adidas.com/com/apps/rtq3so/vp_assets/fonts/adineuePROCyr-Bold.eot?env=&v=18.24');
+          src: local('☺'), url('//www.adidas.com/com/apps/rtq3so/vp_assets/fonts/adineuePROCyr-Bold.woff?env=&v=18.24') format('woff'), url('//www.adidas.com/com/apps/rtq3so/vp_assets/fonts/adineuePROCyr-Bold.ttf?env=&v=18.24') format('truetype');
+          font-weight: bold;
+          font-style: normal;
+      }
+      
+      @font-face {
+          font-family: 'adineueRegular';
+          src: url('//www.adidas.com/com/apps/rtq3so/vp_assets/fonts/adineuePROCyr-Regular.eot?env=&v=18.24');
+          src: local('☺'), url('//www.adidas.com/com/apps/rtq3so/vp_assets/fonts/adineuePROCyr-Regular.woff?env=&v=18.24') format('woff'), url('//www.adidas.com/com/apps/rtq3so/vp_assets/fonts/adineuePROCyr-Regular.ttf?env=&v=18.24') format('truetype');
+          font-weight: normal;
+          font-style: normal;
+      }
+      
+      @font-face {
+          font-family: 'adineuePROBlack';
+          src: url('//www.adidas.com/com/apps/rtq3so/vp_assets/fonts/adineuePRO-BlackWeb.eot?env=&v=18.24');
+          src: local('☺'), url('//www.adidas.com/com/apps/rtq3so/vp_assets/fonts/adineuePRO-BlackWeb.woff?env=&v=18.24') format('woff'), url('//www.adidas.com/com/apps/rtq3so/vp_assets/fonts/adineuePRO-BlackWeb.ttf?env=&v=18.24') format('truetype');
+          font-weight: bold;
+          font-style: normal;
+      }
+      
+      @font-face {
+          font-family: 'icomoon';
+          src: url('//www.adidas.com/com/apps/rtq3so/vp_assets/fonts/icomoon.eot?env=&v=18.24');
+          src: local('☺'), url('//www.adidas.com/com/apps/rtq3so/vp_assets/fonts/icomoon.woff?env=&v=18.24') format('woff'), url('//www.adidas.com/com/apps/rtq3so/vp_assets/fonts/icomoon.ttf?env=&v=18.24') format('truetype');
+          font-weight: normal;
+          font-style: normal;
+      }
+      
+    </style>
+
+
+
+
+  <!-- HTML starte here  -->
+  </head>
+  <body class="custom-c home-c mobile"><!-- CONTENT START -->
+    <div class="kw sold_out"><!-- HEADER START -->
+      <header class="top">
+        <div class="inner">
+    <ul class="menu">
+      <li class="item Jimmy"><a href="https://m.adidas.com/us" data-mobile="https://m.adidas.com/us" data-desktop="https://www.adidas.com/us" class="dynamic-url">Jimmy's Sneaker Store </a></li>
+      </ul>
+          
+          <nav class="menu hidden">
+            <ul class="menu-items">
+           
+              <li class="item men"><a href="https://m.adidas.com/us/men" data-mobile="https://m.adidas.com/us/men" data-desktop="https://www.adidas.com/us/men" class="dynamic-url">  men</a></li>
+              <li class="item women"><a href="https://m.adidas.com/us/women" data-mobile="https://m.adidas.com/us/women" data-desktop="https://www.adidas.com/us/women" class="dynamic-url">  women</a></li>
+              <li class="item kids"><a href="https://m.adidas.com/us/kids" data-mobile="https://m.adidas.com/us/kids" data-desktop="https://www.adidas.com/us/kids" class="dynamic-url">  kids</a></li>
+            </ul>
+          </nav>
+          <aside class="ecom">
+            <ul class="ecom-items">
+              <li class="item my-account"><a href="https://m.adidas.com/on/demandware.store/Sites-adidas-US-Site/en_US/MyAccount-CreateOrLogin" alt="My Account" data-mobile="https://m.adidas.com/on/demandware.store/Sites-adidas-US-Site/en_US/MyAccount-CreateOrLogin" data-desktop="https://www.adidas.com/on/demandware.store/Sites-adidas-US-Site/en_US/MyAccount-CreateOrLogin" class="dynamic-url">&nbsp;</a></li>
+              <li class="item cart"><a href="https://m.adidas.com/on/demandware.store/Sites-adidas-US-Site/en_US/Cart-Show" alt="Cart" data-mobile="https://m.adidas.com/on/demandware.store/Sites-adidas-US-Site/en_US/Cart-Show" data-desktop="https://www.adidas.com/on/demandware.store/Sites-adidas-US-Site/en_US/Cart-Show" class="dynamic-url">&nbsp;</a></li>
+            </ul>
+          </aside>
+        </div>
+      </header><!-- HEADER END -->
+
+
+
+      <section id="logo" class="section">
+        <div class="inner">         
+         <p class="site-title">ADIDAS WEBSITE <br/>BY <br/> JIMMY LIAO </p>       
+          </div> 
+          </section>
+
+      <section class="gallery-cart-wrapper">
+        <div class="inner">
+          <section id="intro" class="section">
+            <div class="inner">              
+            <h1 data-altcopy="product_title" class="name">YEEZY BOOST 350 V2</h1>              
+            <p class="made-by">Cream White</p>
+            </div>
+          </section>    
+
+          <section id="gallery" class="section">
+            <div class="inner">
+              <div class="adult-gallery-wrapper">
+                <div class="shoe-gallery shoe-gallery-adult">
+                  <div class="item"><img data-src="//www.adidas.com/com/apps/rtq3so/vp_assets/images/350-v2/cream-white/adidas_YEEZY_350_V2_AW_Left_adult.jpg?env=&v=18.24" alt="Yeezy" class="lazyOwl"></div>
+                  <div class="item"><img data-src="//www.adidas.com/com/apps/rtq3so/vp_assets/images/350-v2/cream-white/adidas_YEEZY_350_V2_AW_Right_adult.jpg?env=&v=18.24" alt="Yeezy" class="lazyOwl"></div>
+                  <div class="item"><img data-src="//www.adidas.com/com/apps/rtq3so/vp_assets/images/350-v2/cream-white/adidas_YEEZY_350_V2_AW_Top_adult.jpg?env=&v=18.24" alt="Yeezy" class="lazyOwl"></div>
+                  <div class="item"><img data-src="//www.adidas.com/com/apps/rtq3so/vp_assets/images/350-v2/cream-white/adidas_YEEZY_350_V2_AW_Front_adult.jpg?env=&v=18.24" alt="Yeezy" class="lazyOwl"></div>
+                </div>
+              </div>
+              <div class="hidden infant-gallery-wrapper">
+                <div class="shoe-gallery shoe-gallery-infant hidden">
+                  <div class="item"><img data-src="//www.adidas.com/com/apps/rtq3so/vp_assets/images/350-v2/cream-white/adidas_YEEZY_350_V2_AW_Left_infant.jpg?env=&v=18.24" alt="Yeezy" class="lazyOwl"></div>
+                  <div class="item"><img data-src="//www.adidas.com/com/apps/rtq3so/vp_assets/images/350-v2/cream-white/adidas_YEEZY_350_V2_AW_Right_infant.jpg?env=&v=18.24" alt="Yeezy" class="lazyOwl"></div>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section id="shoe" class="section">
+            <div class="inner">
+              <div class="state">                
+              <div class="sold_out section">
+          <p data-altcopy="product_sold_out_message">USD 220 </p>
+
+              <p class="select-item select-item-1">ADULT</p>
+<p class="select-item select-item-2 hidden">INFANT</p>
+<ul class="item-picker">
+  <li data-item="adult" data-id="1" class="item item-1 active">ADULT</li>
+  <li data-item="infant" data-id="2" class="item item-2">INFANT</li>
+              <p data-altcopy="product_sold_out_message">SELECT A SIZE BELOW</p>
+              <div class="add-product-block ">
+
+
+  <select name="pid" id="size-select-BA7208" class="size-select">
+    <option value="empty">Select size</option>
+        <ispagecontextset name="product_size*" override="true" value="">
+      
+            <option 
+            >
+              5
+            </option>
+              <option 
+            >
+              6
+            </option>
+              <option 
+            >
+              7
+            </option>
+              <option 
+            >
+              8
+            </option>
+              <option 
+            >
+              9
+            </option>
+              <option 
+            >
+              10
+            </option>
+              <option 
+            >
+              11
+            </option>
+              <option 
+            >
+              12
+            </option>
+              <option 
+            >
+              13
+            </option>
+              <option 
+            >
+              14
+            </option>
+              <option 
+            >
+              15
+            </option>
+
+  </select>
+</div>
+
+
+
+    
+
+
+
+
+
+
+
+      <!-- Add the size and the facial recogntion here -->
+
+   <!-- faical recogniton from here -->
+
+
+
+
+    <button type="button" id="webcamModule" onclick="webcamModule();"> Checkout </button>
+
+    <video id="webcamVideo"></video>
+    <div id="showCounter"></div>
+    <div id="detectResponse"></div>
+
+
+
+
+
+
+<!-- Script -->
+
+<script>
+var webcamModule = function () {
+    var streaming = false;
+    var video = null;
+    (function () {
+        video = document.getElementById('webcamVideo');
+        navigator.getUserMedia({
+            video: true,
+            audio: false
+        },
+            function(stream) {
+                if (navigator.mozGetUserMedia) {
+                  video.mozSrcObject = stream;
+                } 
+                else {
+                    var vendorURL = window.URL || window.webkitURL;
+                    video.src = vendorURL.createObjectURL(stream);
+                }
+                video.play();
+                localStream = stream.getTracks()[0];
+            },
+            function(err) {
+                console.log(err)
+            }
+        );
+        video.addEventListener('canplay', function(ev){
+            if (!streaming) {
+                video.setAttribute('width', '600');
+                video.setAttribute('height', '450');
+                streaming = true;
+            }
+            var captureInterval = 3000;
+            var countdown = captureInterval/1000;
+            var counterFunction = setInterval(function () {
+                $("#showCounter").html(countdown);
+                if (countdown <= 0) {
+                    takepicture(video);
+                    clearInterval(counterFunction);
+                    localStream.stop();
+                }
+                countdown --;
+            },1000);
+        },  false);
+    })();
+};
+var takepicture = function(video) {
+    $("#showCounter").html("Loading...");
+    var canvas = document.createElement('CANVAS');
+    var context = canvas.getContext('2d');
+    canvas.width = '600';
+    canvas.height = '450';
+    // draw video image onto canvas, get data
+    context.drawImage(video, 0, 0);
+    var imageData = canvas.toDataURL('image/png');
+    var data = {};
+    imgObj = { 
+        "image"   : parseImageData(imageData)
+    };
+    data.imgObj = JSON.stringify(imgObj);
+    $.ajax({
+        type: 'POST',
+        url: 'detect.php',
+        data: data,
+        dataType: 'text'
+    }).done(function(data){
+
+     
+        var response = JSON.parse(data);
+        if (response.images) {
+          alert("checkout successfully!");
+
+        }
+        else {
+            alert("fail");
+        }
+    });
+    $(video).hide();
+}
+var parseImageData = function(imageData) {
+    imageData = imageData.replace("data:image/jpeg;base64,", "");
+    imageData = imageData.replace("data:image/jpg;base64,", "");
+    imageData = imageData.replace("data:image/png;base64,", "");
+    imageData = imageData.replace("data:image/gif;base64,", "");
+    imageData = imageData.replace("data:image/bmp;base64,", "");
+    return imageData;
+}
+
+
+
+</script>
+
+
+     <!-- End of facial recognition -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            </div>
+          </section>
+          <div class="clearfix"></div>
+        </div>
+      </section>            <section id="description" class="section">
+        <div class="inner">
+                              <p data-altcopy="product_description">THE YEEZY BOOST 350 V2 FEATURES AN ALL WHITE PRIMEKNIT UPPER, DISTINCT CENTER STITCHING, AND HEEL TAB. A SEMI-TRANSLUCENT RUBBER OUTSOLE AND TPU SIDEWALLS CREATE A STRIKING EFFECT WHILE PROVIDING SUPERIOR TRACTION, WHILE THE YEEZY BOOST 350 V2 MIDSOLE UTILIZES ADIDAS’ INNOVATIVE BOOST™ TECHNOLOGY TO CREATE A DURABLE, SHOCK-RESISTANT, RESPONSIVE SOLE.</p>                  
+        </div>
+
+      </section>            <section id="signup" class="section">
+        <div class="inner">          <div class="copy-wrapper">
+            <p data-altcopy="product_name" class="name">YEEZY BOOST 350 V2</p>
+            <p class="coming">THERE IS MORE COMING FROM ADIDAS ORIGINALS AND KANYE WEST. SIGN UP TO BE KEPT UP TO DATE.</p>
+          </div><a href="https://ssf.adidas.com/cep/dc.action?campaignId=2461&country=US&lid=EN&cm_sp=KANYE_YEEZY-_-MASTHEAD-_-GET_UPDATES" data-desktop="" data-mobile="https://ssf.adidas.com/cep/dc.action?campaignId=2461&country=US&lid=EN&cm_sp=KANYE_YEEZY-_-MASTHEAD-_-GET_UPDATES" target="_blank" class="btn get-updates">GET UPDATES            <!--img(src!="//www.adidas.com/com/apps/rtq3so/vp_assets/images/o-arrow-right-black.png?env=&v=18.24" width="14" height="14")--></a>        
+      </div>
+     
+
+
+</body>
+</html>
